8d2be5c99a9022ef06d6dabb05a969523d4fc72e
Hov-007 <69868604+Hov-007@users.noreply.github.com>
2020-08-25 22:10:17 +0400

    Add project files.

---
+++ ZIl-Homework2.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZIl_Homework2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte byt = 200;
+            sbyte a = 120;
+            short b = 30000;
+            ushort c = 60000;
+            int d = 30000000;
+            uint e = 1000000000;
+            long f = 100000000000;
+            ulong g = 1000000000000;
+            float h = 1.123F;
+            double i = 123.75;
+            decimal j = 123.45678m;
+            string k = "name";
+            bool l = true;
+            char m = '$';
+        }
+    }
+}
---
+++ ZIl-Homework2.3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZIl_Homework2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Firstname = "Hovhannes";
+            string Lastname = "Aleksanyan";
+            Console.WriteLine(Lastname + " " + Firstname);
+        }
+    }
+}
