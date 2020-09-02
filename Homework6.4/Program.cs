2da1274bd15abae4c6201ee2d13754448874cd0c
Hov-007 <69868604+Hov-007@users.noreply.github.com>
2020-09-03 03:20:42 +0400

    MInor fix

--- Homework6.4/Program.cs
+++ Homework6.4/Program.cs
@@ -4,17 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Homework6._4
+namespace Homework6._3
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("ENter a natural number");
-            int number = int.Parse(Console.ReadLine());
-            for(int i = 1; i <= 9; i++)
+            int n= int.Parse(Console.ReadLine());
+            for(int i = 1; i <= n;n++)
             {
-                Console.WriteLine(Math.Pow(number, i));
+                Console.WriteLine(i);
             }
         }
     }
