0916f89f1e8077f1218a8ce4dc174200bb16ae9b
Hov-007 <69868604+Hov-007@users.noreply.github.com>
2020-09-04 19:48:42 +0400

    Homework6

--- Homework6.4/Program.cs
+++ Homework6.4/Program.cs
@@ -9,19 +9,10 @@ namespace Homework6._3
     class Program
     {
         static void Main(string[] args)
-        {
-            //First method(without function)
-            Console.WriteLine("Enter a one-figure number");
+        {   
+           Console.WriteLine("Enter a one-figure number");
             int n = int.Parse(Console.ReadLine());
-            int i = 1;
-            while (i <= 9)
-            {
-                Console.WriteLine(n * n);
-                i += 1;
-            }
-
-            //Second method(using function math.pow)
-            for (i = 1; i <= 9; i++)
+            for (int i = 1; i <= 9; i++)
             {
                 Console.WriteLine(Math.Pow(n, i));
             }
