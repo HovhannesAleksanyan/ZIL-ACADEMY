87524ff3f98befbdc402660f914d6784e3a5bc70
Hov-007 <69868604+Hov-007@users.noreply.github.com>
2020-09-06 02:55:13 +0400

    Homework7

---
+++ HOmewrok7/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace HOmewrok7
+{
+    class Program
+    {static void Task6()
+        {
+            Console.WriteLine("Enter two numbers");
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            for (int i = A; i <= B; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(B - A + 1);
+        }
+        static void Task7()
+        {
+            Console.WriteLine("Enter two numbers");
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = A; i <= B; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+        static void TAsk8()
+        {
+            Console.WriteLine("Enter two numbers");
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            for(int i = A; i <= B; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+        }
+        static void Task9()
+        {
+            Console.WriteLine("Enter a anumber");
+            double N = Double.Parse(Console.ReadLine());
+            double sum = 0;
+            for (double i = 1; i <= N; i++)
+            {
+                sum += 1 / i;
+            }
+            Console.WriteLine(sum);
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
