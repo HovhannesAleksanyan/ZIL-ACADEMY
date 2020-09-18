e8ded6963cba99229900bf0ceded224ca95b2c74
Hov-007 <69868604+Hov-007@users.noreply.github.com>
2020-09-18 13:17:08 +0400

    Homework11

---
+++ Class/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car lamborghini = new car();
+            lamborghini.color = "red";
+            lamborghini.enginetype = "petrol 6 litres";
+            lamborghini.seatsquantity = 2;
+
+            bike BMW = new bike();
+            BMW.color = "white";
+            BMW.enginetype = "petrol 3 litres biturbo ";
+            BMW.seatsquantity = 1;
+            BMW.wheelquantity = 2;
+
+            train train = new train();
+            train.color = "black";
+            train.color = "diesel 8 litres";
+            train.seatsquantity = 300;
+            train.wheelquantity = 20;
+
+            airplane boing = new airplane();
+            boing.color = "white";
+            boing.enginetype = "10 litres diesel";
+            boing.seatsquantity = 100;
+            boing.wheelquantity = 3;
+
+            Ship Titanic = new Ship();
+            Titanic.color = "white";
+            Titanic.enginetype = "turbo diesel 6 litres";
+            Titanic.seatsquantity = 100;
+            Titanic.wheelquantity = 0;
+        }
+    }
+}
---
+++ Class/transportation.cs
@@ -0,0 +1,31 @@
+﻿using Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Transportation
+    {
+        public string color;
+        public string enginetype;
+        public int wheelquantity;
+        public int seatsquantity;
+        public void Goahead()
+        {
+
+        }
+        public void Gobackwards()
+        {
+
+        }
+        public void Stop()
+        {
+
+        }
+
+    }
+}
